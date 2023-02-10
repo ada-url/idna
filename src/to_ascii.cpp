c2c0c60661708af452f00c0a9941348945390713
Daniel Lemire <daniel@lemire.me>
2023-02-10 17:51:49 -0500

    Various obvious optimizations.

--- src/to_ascii.cpp
+++ src/to_ascii.cpp
@@ -17,6 +17,13 @@ bool begins_with(std::u32string_view view, std::u32string_view prefix) {
   return view.substr(0, prefix.size()) == prefix;
 }
 
+bool begins_with(std::string_view view, std::string_view prefix) {
+  if (view.size() < prefix.size()) {
+    return false;
+  }
+  return view.substr(0, prefix.size()) == prefix;
+}
+
 bool is_ascii(std::u32string_view view) {
   for (uint32_t c : view) {
     if (c >= 0x80) {
@@ -26,12 +33,70 @@ bool is_ascii(std::u32string_view view) {
   return true;
 }
 
+bool is_ascii(std::string_view view) {
+  for (uint8_t c : view) {
+    if (c >= 0x80) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// We return "" on error. For now.
+static std::string from_ascii_to_ascii(std::string_view ut8_string) {
+  static const std::string error = "";
+  // copy and map
+  // we could be more efficient by avoiding the copy when unnecessary.
+  std::string mapped_string = std::string(ut8_string);
+  ascii_map(mapped_string.data(), mapped_string.size());
+  std::string out;
+  size_t label_start = 0;
+
+  while (label_start != mapped_string.size()) {
+    size_t loc_dot = mapped_string.find('.', label_start);
+    bool is_last_label = (loc_dot == std::string_view::npos);
+    size_t label_size =
+        is_last_label ? mapped_string.size() - label_start : loc_dot - label_start;
+    size_t label_size_with_dot = is_last_label ? label_size : label_size + 1;
+    std::string_view label_view(mapped_string.data() + label_start, label_size);
+    label_start += label_size_with_dot;
+    if (label_size == 0) {
+      // empty label? Nothing to do.
+    } else if (begins_with(label_view, "xn--")) {
+      // The xn-- part is the expensive game.
+      out.append(label_view);
+      std::string_view puny_segment_ascii(
+          out.data() + out.size() - label_view.size() + 4,
+          label_view.size() - 4);
+      std::u32string tmp_buffer;
+      bool is_ok = ada::idna::punycode_to_utf32(puny_segment_ascii, tmp_buffer);
+      if (!is_ok) {
+        return error;
+      }
+      std::u32string post_map = ada::idna::map(tmp_buffer);
+      if(tmp_buffer != post_map) { return error; }
+      std::u32string pre_normal = post_map;
+      normalize(post_map);
+      if(post_map != pre_normal) { return error; }
+      if (post_map.empty()) {
+        return error;
+      }
+      if (!is_label_valid(post_map)) {
+        return error;
+      }
+    } else {
+      out.append(label_view);
+    }
+    if (!is_last_label) {
+      out.push_back('.');
+    }
+  }
+  return out;
+}
+
 // We return "" on error. For now.
 std::string to_ascii(std::string_view ut8_string) {
-  // If the string is pure ascii, then **we do not need** to convert to
-  // UTF-32 and could use a faster path where we only do verify_punycode
-  // where needed. Though we may need to do mapping and check for
-  // forbidden characters.
+  if(is_ascii(ut8_string)) { return from_ascii_to_ascii(ut8_string); }
   static const std::string error = "";
   // We convert to UTF-32
   size_t utf32_length =
@@ -56,10 +121,8 @@ std::string to_ascii(std::string_view ut8_string) {
     label_start += label_size_with_dot;
     if (label_size == 0) {
       // empty label? Nothing to do.
-    } else if (begins_with(label_view, U"xn--") ||
-               begins_with(label_view, U"XN--") ||
-               begins_with(label_view, U"Xn--") ||
-               begins_with(label_view, U"xN--")) {
+    } else if (begins_with(label_view, U"xn--")) {
+      // we do not need to check, e.g., Xn-- because mapping goes to lower case
       for (char32_t c : label_view) {
         if (c >= 0x80) {
           return error;
@@ -86,14 +149,19 @@ std::string to_ascii(std::string_view ut8_string) {
         return error;
       }
     } else {
-      if (!is_label_valid(label_view)) {
-        return error;
-      }
+      // The fast path here is an ascii label.
       if (is_ascii(label_view)) {
+        // no validation needed.
         for (char32_t c : label_view) {
           out += (unsigned char)(c);
         }
       } else {
+        // slow path.
+        // first check validity.
+        if (!is_label_valid(label_view)) {
+          return error;
+        }
+        // It is valid! So now we must encode it as punycode...
         out.append("xn--");
         bool is_ok = ada::idna::utf32_to_punycode(label_view, out);
         if (!is_ok) {

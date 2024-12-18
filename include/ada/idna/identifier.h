05bad1223d5fd644931af4d599574ef37c0f5522
Daniel Lemire <daniel@lemire.me>
2024-12-17 20:34:14 -0500

    more comments...

--- include/ada/idna/identifier.h
+++ include/ada/idna/identifier.h
@@ -10,6 +10,9 @@ namespace ada::idna {
 // Verify if it is valid name code point given a Unicode code point and a boolean first:
 // If first is true return the result of checking if code point is contained in the IdentifierStart set of code points.
 // Otherwise return the result of checking if code point is contained in the IdentifierPart set of code points.
+// Returns false if the input is empty or the code point is not valid.
+// There is minimal Unicode error handling: the input should be valid UTF-8.
+// https://urlpattern.spec.whatwg.org/#is-a-valid-name-code-point
 bool valid_name_code_point(std::string_view input, bool first);
 
 }  // namespace ada::idna

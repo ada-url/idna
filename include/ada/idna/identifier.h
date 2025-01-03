2d18c92b59a97bff9d311e593b3dfbd7831dcede
Daniel Lemire <daniel@lemire.me>
2025-01-03 11:54:29 -0500

    update verifier API (#53)

--- include/ada/idna/identifier.h
+++ include/ada/idna/identifier.h
@@ -6,7 +6,6 @@
 
 namespace ada::idna {
 
-// Access the first code point of the input string.
 // Verify if it is valid name code point given a Unicode code point and a
 // boolean first: If first is true return the result of checking if code point
 // is contained in the IdentifierStart set of code points. Otherwise return the
@@ -14,7 +13,7 @@ namespace ada::idna {
 // code points. Returns false if the input is empty or the code point is not
 // valid. There is minimal Unicode error handling: the input should be valid
 // UTF-8. https://urlpattern.spec.whatwg.org/#is-a-valid-name-code-point
-bool valid_name_code_point(std::string_view input, bool first);
+bool valid_name_code_point(char32_t code_point, bool first);
 
 }  // namespace ada::idna
 

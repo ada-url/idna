f88dee6cc62e19b93ef196b28d55f7df3f02fc8f
Carlos Sousa <carlosedu.eduardo99@gmail.com>
2025-06-24 19:48:02 -0300

    add option to use simdutf for unicode transcoding

--- src/to_unicode.cpp
+++ src/to_unicode.cpp
@@ -7,6 +7,10 @@
 #include "ada/idna/unicode_transcoding.h"
 #include "idna.h"
 
+#ifdef ADA_USE_SIMDUTF
+#include "simdutf.h"
+#endif
+
 namespace ada::idna {
 std::string to_unicode(std::string_view input) {
   std::string output;
@@ -25,11 +29,19 @@ std::string to_unicode(std::string_view input) {
       if (ada::idna::verify_punycode(label_view)) {
         std::u32string tmp_buffer;
         if (ada::idna::punycode_to_utf32(label_view, tmp_buffer)) {
+#ifdef ADA_USE_SIMDUTF
+          auto utf8_size : size_t simdutf::utf8_length_from_utf32(
+                               tmp_buffer.data(), tmp_buffer.size());
+          std::string final_utf8(utf8_size, '\0');
+          simdutf::convert_utf32_to_utf8(tmp_buffer.data(), tmp_buffer.size(),
+                                         final_utf8.data());
+#else
           auto utf8_size = ada::idna::utf8_length_from_utf32(tmp_buffer.data(),
                                                              tmp_buffer.size());
           std::string final_utf8(utf8_size, '\0');
           ada::idna::utf32_to_utf8(tmp_buffer.data(), tmp_buffer.size(),
                                    final_utf8.data());
+#endif
           output.append(final_utf8);
         } else {
           // ToUnicode never fails.  If any step fails, then the original input
